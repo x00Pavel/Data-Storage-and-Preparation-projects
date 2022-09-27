036b9c3b27f70cb7947918d6ab5cec45fd4d39ba
x00Pavel <pavel.yadlouski@gmail.com>
2022-09-27 18:27:38 +0200

    Update downloading and unziping files

--- projekt-1/src/get-zip.py
+++ projekt-1/src/get-zip.py
@@ -1,41 +1,83 @@
-from cgitb import reset
-import re
 import requests
 from urllib.request import urlopen
 from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 import bs4
 from dateutil.parser import parse
-from os.path import basename
+from subprocess import check_output
+from logging import getLogger
+
+thread_log = getLogger("thread_logger")
+thread_log.setLevel("DEBUG")
 
 base_source_url = "https://portal.cisjr.cz"
 base_source_path = f"{base_source_url}/pub/draha/celostatni/szdc/2022/"
-base_path = Path("../data/")
+base_path = Path("data/")
 base_path.mkdir(exist_ok=True)
 
 
-def download_files():
-    def worker(url):
-        # Get list of file URLs in the directory
-        sub_dir = Path(f"{base_path}/{url[1]}")
-        sub_dir.mkdir(exist_ok=True)
+def unzip_files(dir: Path):
+    """
+    Unzip all files in a directory. Files are parsed GZIP compressed files with
+    .zip extension. This implementation uses `gunzip` command line tool.
 
-        r = requests.get(url)
-        assert r.ok, f"GET {url} failed with {r.status_code}"
-        data = bs4.BeautifulSoup(r.text, "html.parser")
+    :param dir: directory with files to unzip
+    :type dir: pathlib.Path
+    """
+    for f in dir.iterdir():
+        cmd = ["gunzip", "-S", ".zip"]
+        check_output(cmd + [str(f)])
+    thread_log.info(f"Unzipped all files in {dir}")
 
-        # Download all files in a directory
-        status_codes = []
-        for l in data.find_all("a"):
-            with urlopen(base_source_url + l["href"]) as f:
-                zip_data = f.read()
-            assert zip_data
-            with open(Path(sub_dir, l["href"]).name, "wb") as zip_f:
-                zip_f.write(zip_data)
 
-            status_codes.append(r.status_code)
-        return status_codes
+def download_files_in_dir(data: bs4.BeautifulSoup, dir: Path):
+    """
+    Download all files form a directory. Directory by itself has to be created
+    before. Files are parsed from bs4.BeautifulSoup object using href attribute
+    of <a> tag.
 
+    :param data: HTML page with directory content
+    :type data: bs4.BeautifulSoup
+    :param dir: directory where to download files
+    :type dir: pathlib.Path
+    """
+
+    for l in data.find_all("a"):
+        if not l.text.endswith(".zip"): continue
+        with urlopen(base_source_url + l["href"]) as f:
+            zip_data = f.read()
+        assert zip_data
+        with dir.joinpath(l.text).open("wb") as zip_f:
+            zip_f.write(zip_data)
+    thread_log.info(f"Downloaded all files into {dir}")
+
+
+def worker(url):
+    """
+    Worker function for ThreadPoolExecutor
+    
+    :param url: tuple of (url, dir_name)
+    """
+    
+    # Get list of file URLs in the directory
+    sub_dir = Path(f"{base_path}/{url[1]}")
+    sub_dir.mkdir(exist_ok=True)
+
+    r = requests.get(url[0])
+    assert r.ok, f"GET {url[0]} failed with {r.status_code}"
+    data = bs4.BeautifulSoup(r.text, "html.parser")
+
+    # Download all files in a directory
+    download_files_in_dir(data, sub_dir)
+    unzip_files(sub_dir)
+    thread_log.info(f"Finished processing {url[0]}")
+
+
+def prepare_files():
+    """
+    Prepare files for further processing meaning download files and extract file
+    from an archive. Implemented using multithreading.
+    """
     # Download content of source dir
     dir_content = requests.get(base_source_path)
     assert dir_content.ok, f"GET request for {base_source_path} is not successful"
@@ -50,21 +92,14 @@ def download_files():
         except:
             print(f"Not a directory {d.text}")
 
-    # Recursively download all from each directory
-    print(links)
-    with ThreadPoolExecutor(max_workers=10) as executor:
-        result = executor.map(worker, links)
+    # Recursively download all from each directory and gunzip it
+    with ThreadPoolExecutor(max_workers=11) as executor:
+        executor.map(worker, links)
 
-    return result
-
-
-def unzip_files(files):
-    ...
 
 
 def main():
-    files = download_files()
-    unzip_files(files)
+    prepare_files()
 
 
 if __name__ == "__main__":

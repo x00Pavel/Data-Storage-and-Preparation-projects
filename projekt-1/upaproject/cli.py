03bdb8342d3be03e928c9a8b81ce5dab5c14302d
x00Pavel <pavel.yadlouski@gmail.com>
2022-10-07 19:46:09 +0200

    Add more options to CLI

--- projekt-1/upaproject/cli.py
+++ projekt-1/upaproject/cli.py
@@ -3,30 +3,39 @@ import logging
 
 import click
 
-from upaproject import thread_log
+from upaproject import thread_log, handler
 from upaproject.downloader import Downloader
+from upaproject.updater import update_documents
 
-# from upaproject import update_object
-
+from mongoengine import connect
+from os import environ
 
 @click.group()
 @click.option("--debug/--no-debug", default=False)
-def cli(debug):
+@click.option("-c", "--connection",
+              type=click.Choice(['local', 'remote']),
+              default="local"
+            )
+@click.option("-d", "--db", default="upa")
+def cli(debug, connection, db):
     if debug:
+        handler.setLevel(logging.DEBUG)
         thread_log.setLevel(logging.DEBUG)
     else:
+        handler.setLevel(logging.DEBUG)
         thread_log.setLevel(logging.INFO)
-    pass
+    url = environ["MONGO_LOCAL_URI"] if connection == "local" else environ["MONGO_URI"]
+    result = connect(db, host=url)
+    thread_log.info(f"Connected to {url}")
+    
 
 
 @click.command(help="Update database from source web")
 @click.option("-d","--db", default="upa", help="Database name",
               show_default=True)
-@click.option("-t", "--connection-type", default="local", help="Connection type",
-              show_default=True)
-def update(db, connection_type):
+def update(db):
     Downloader.prepare_files()
-    # update_object()
+    # update_documents()
 
 @click.command(help="Find the connections between two stations")
 @click.option("-f","--from", "from_", help="Start point of the route",

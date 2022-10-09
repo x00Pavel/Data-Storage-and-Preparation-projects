a5f99929fd487d14358117854e39f220db1d4edc
x00Pavel <pavel.yadlouski@gmail.com>
2022-10-09 22:21:33 +0200

    Add method for getting id from text fro Location

--- projekt-1/upaproject/models/location.py
+++ projekt-1/upaproject/models/location.py
@@ -5,6 +5,7 @@ from mongoengine import (DynamicDocument, LazyReferenceField, ListField,
 from bson.objectid import ObjectId
 
 class Location(DynamicDocument):
+    # meta = {'collection': 'locations'}
     meta = {'collection': 'stations'}
     
     location_id = ObjectIdField(required=True, primary_key=True)
@@ -19,14 +20,19 @@ class Location(DynamicDocument):
         self.country = xml_data.find("CountryCodeISO").text
 
     @staticmethod
-    def gen_id_from_xml(xml_data: ET):
-        id_text = xml_data.find("LocationPrimaryCode").text
+    def get_id_from_text(id_text: str):
         while len(id_text) < 12:
             id_text += '0'
-        return ObjectId(bytes(id_text, 'utf-8'))
+        return ObjectId(bytes(id_text, "utf-8"))
+
+    @staticmethod
+    def gen_id_from_xml(xml_data: ET):
+        id_text = xml_data.find("LocationPrimaryCode").text
+        return Location.get_id_from_text(id_text)
+
 
     def __str__(self):
-        return f"Station {self.location_name} ({self.location_id}) in {self.country}"
+        return f"Station {self.location_name} ({self.location_id_text}) in {self.country} (OID: {self.location_id})"
 
     def __repr__(self):
         return self.__str__()

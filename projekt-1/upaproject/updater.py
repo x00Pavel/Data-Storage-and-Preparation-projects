94ca48cd5ac1a60b952ec32f51e3ebf3c87f6ed1
x00Pavel <pavel.yadlouski@gmail.com>
2022-10-16 18:35:19 +0200

    Separate parsing of planned and cancelled messages

--- projekt-1/upaproject/updater.py
+++ projekt-1/upaproject/updater.py
@@ -1,23 +1,27 @@
+from cmath import log
 from concurrent.futures import ThreadPoolExecutor
+from enum import unique
 from pathlib import Path
+from traceback import print_tb
 from xml.etree import ElementTree as ET
 
 from dateutil.parser import parse
 
-from upaproject import data_base_path, module_root, progress_log, thread_log
+from upaproject import data_base_path, module_root, default_logger as logger
 from upaproject.models.calendar import Calendar
 from upaproject.models.connection import Connection
 from upaproject.models.location import Location
 from upaproject.models.station import Station
 from upaproject.models.train import Train
+from upaproject.models.cancellation import Cancellation
 
 
 def parse_stations(conn, station_iter):
-    progress_log.info(f"Start processing stations for connection {conn.connection_id_text}\n")
+    logger.info(f"Start processing stations for connection {conn.connection_id_text}")
     for entry in station_iter:
-        station = Station().from_xml(entry)
         location_xml = entry.find("Location")
         id_ = Location.gen_id_from_xml(location_xml)
+
         location = Location.objects(location_id=id_)
         if location:
             location = location.first()
@@ -25,6 +29,7 @@ def parse_stations(conn, station_iter):
             location = Location()
             location.from_xml(location_xml)
             location.location_id = id_
+        station = Station().from_xml(entry)
 
         
         if conn.connection_id not in location.connections:
@@ -33,68 +38,103 @@ def parse_stations(conn, station_iter):
         location.save()
         conn.stations.append(station)
     
-    progress_log.info(f"Finish processing stations (#{len(conn.stations)}) for connection {conn.connection_id_text}\n")
+    # logger.info(f"Finish processing stations (#{len(conn.stations)}) for connection {conn.connection_id_text}")
 
-def parse_xml(file: Path):
-    root = ET.parse(file).getroot()
-    ids = root.find("Identifiers")
 
-    if ids is None:
-        # thread_log.warning(f"File {str(file)} has no Identifiers")
-        return
-    ids = ids.findall("PlannedTransportIdentifiers")
+def parse_identifiers(identifiers):
+    result = dict()
+    result["pa_id"], result["tr_id"]  = identifiers.findall("PlannedTransportIdentifiers")
+    result["related_id"] = identifiers.find("RelatedPlannedTransportIdentifiers")
+    if result["related_id"]:
+        logger.debug("Found related transport")
+    return result
 
-    pa_id = ids[0]
-    tr_id = ids[1]
-    related_id = ids[2] if len(ids) == 3 else None
 
-    # Initialize the Connection document
-    conn_id_text, conn_id = Connection.gen_id_from_xml(pa_id)
-    progress_log.info(f"Start processing connection {conn_id_text}\n")
+def parse_planned(root):
+    ids = parse_identifiers(root.find("Identifiers"))
+    conn_id_text, conn_id = Connection.gen_id_from_xml(ids["pa_id"])
+    logger.info(f"Start processing connection {conn_id_text}")
+
     conn = Connection.objects(connection_id = conn_id)
     if conn:
-        progress_log.info(f"Found existing connection with id {conn_id} (in text form {conn_id_text})\n")
+        logger.info(
+            f"Found existing connection with id {conn_id} " \
+            f"(in text form {conn_id_text})")
         conn = conn.first()
     else:
         conn = Connection()
-    conn.connection_id_text, conn.connection_id = conn.gen_id_from_xml(pa_id)
-    conn.train = Train().from_xml(tr_id)
-    params = root.findall("NetworkSpecificParameter")
-    if params:
-        for p in map(lambda p: ET.tostring(p, encoding="unicode"), params):
-            conn.spec_params.append(p)
+        conn.connection_id_text, conn.connection_id = conn_id_text, conn_id
+        conn.train = Train().from_xml(ids["tr_id"])
+        params = root.findall("NetworkSpecificParameter")
+        if params:
+            # Lambda: convert ElementTree to string with unicode encoding
+            for p in map(lambda p: ET.tostring(p, encoding="unicode"), params):
+                conn.spec_params.append(p)
 
-    conn.header = root.find("CZPTTHeader").text
-    conn.creation = parse(root.find("CZPTTCreation").text)
-    if related_id:
-        conn.related = Connection.gen_id_from_xml(related_id)
-
+        conn.header = root.find("CZPTTHeader").text
+        conn.creation = parse(root.find("CZPTTCreation").text)
+    if ids["related_id"]:
+        conn.related = Connection.gen_id_from_xml(ids["related_id"])[1]
+    
     # Parse all CZPTTLocation elements
     information = root.find("CZPTTInformation")
     parse_stations(conn, information.iter("CZPTTLocation"))
-    print(conn.stations[0].location.fetch())
     conn.start = conn.stations[0].location.fetch().to_dbref()
     conn.end = conn.stations[-1].location.fetch().to_dbref()
     conn.calendar = Calendar().from_xml(information.find("PlannedCalendar"))
     conn.save()
-    progress_log.info(f"Finish processing connection {conn_id_text}\n")
-    return
+    logger.info(f"Finish processing connection {conn_id_text}")
 
 
-def worker(dir_name: Path):
-    thread_log.info(f"Starting worker for {dir_name}")
+def parse_canceled(root):
+    logger.warning("Start processing Cancelled connection")
+    ids = root.findall("PlannedTransportIdentifiers")
+    cancellation = Cancellation()
+    cancellation.connection_id = Connection.gen_id_from_xml(ids[0])[1]
+    cancellation.train = Train().from_xml(ids[1])
+    cancellation.calendar = Calendar().from_xml(root.find("PlannedCalendar"))
+    cancellation.save()
+    logger.warning("Finish processing Cancelled connection")
+    
+
+def parse_xml(file: Path):
+    logger.warning(f"Parsing {str(file)}")
+    root = ET.parse(file).getroot()
+    if root.tag == "CZPTTCISMessage":
+        parse_planned(root)
+    elif root.tag == "CZCanceledPTTMessage":
+        parse_canceled(root)
+    
+
+
+def worker(files_list):
+    logger.warning(f"Starting worker with {len(files_list)} files")
     processed = 0
-    for f in dir_name.iterdir():
+    for f in files_list:
         if f.suffix != ".xml":
-            thread_log.warning(f"Not a XML file: {str(f)}")
+            logger.warning(f"Not a XML file: {str(f)}")
             continue
-        thread_log.info(f"Processing {str(f)}")
+        logger.info(f"Processing {str(f)}")
         parse_xml(f)
         processed += 1
-        progress_log.info(f"Processed {processed} files in {dir_name}\r")
-    thread_log.info(f"Processed {processed} files in {dir_name}")
+    #     logger.info(f"Processed {processed} files in {dir_name}\r")
+    logger.warning(f"Processed {processed} files")
+
 
 def update_documents():
-    lst = [module_root.joinpath("gvd")] + list(data_base_path.iterdir())
-    with ThreadPoolExecutor(max_workers=len(lst)) as executor:
-        executor.map(worker, lst)
+    thread_num = 30 
+    lst = list(data_base_path.iterdir())
+    # lst = [Path("/home/plovec/studies/fit/mitai/2-rocnik/upa/projekt/projekt-1/data/2021-12")]
+    all_files = []
+    for d in lst:
+        all_files += [f for f in d.iterdir() if f.suffix == ".xml"]
+    files_num = len(all_files)
+    per_thread = files_num // thread_num
+    logger.warning(f"Found {files_num} files. Per thread: {per_thread}")
+    # Create list of chunks that contains files for processing in individual threads
+    # for f in all_files:
+    #     parse_xml(f)
+    chunks = [all_files[x:x+per_thread] for x in range(0, files_num, per_thread)]
+    logger.critical(f"Starting {thread_num} threads")
+    with ThreadPoolExecutor(max_workers=thread_num) as executor:
+        executor.map(worker, chunks)

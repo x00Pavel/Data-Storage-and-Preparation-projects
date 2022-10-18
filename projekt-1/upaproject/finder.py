86819c1e27891d0a0e6c5ae06cd79d969dad7f55
x00Pavel <pavel.yadlouski@gmail.com>
2022-10-18 21:48:44 +0200

    Add check for canceled connections

--- projekt-1/upaproject/finder.py
+++ projekt-1/upaproject/finder.py
@@ -1,14 +1,20 @@
+from calendar import calendar
 from bitmap import BitMap
 from dateutil.parser import parse
 
 from upaproject import get_intersac_pipeline, default_logger as logger
-from upaproject.models import connection, location, station
+from upaproject.models import cancellation, connection, location, station
 
 def check_bitmap_for_date(date, calendar):
-    bitmap = BitMap.fromstring(calendar["bitmap"])
     logger.debug(f"Checking bitmap for {date}")
-    logger.debug(f"Validity period: {calendar['start_date']} - {calendar['end_date']}")
-    days_count = (date - calendar["start_date"]).days
+    if type(calendar) == dict:
+        bitmap = BitMap.fromstring(calendar["bitmap"])
+        logger.debug(f"Validity period: {calendar['start_date']} - {calendar['end_date']}")
+        days_count = (date - calendar["start_date"]).days
+    else:
+        bitmap = BitMap.fromstring(calendar.bitmap)
+        logger.debug(f"Validity period: {calendar.start_date} - {calendar.end_date}")
+        days_count = (date - calendar.start_date).days
     logger.debug(f"Number of days {days_count}")
     logger.debug(f"Bit on position {days_count}: {bitmap[days_count]}")
     return bitmap.test(days_count)
@@ -31,15 +37,17 @@ def find_correct_direction(connections, from_id, to_id):
 def find_connection(from_, to_, date):
     date_time = parse(date)
     logger.debug(f"Looking for connection from {from_} to {to_} on {date_time}")
-    
-    from_id = location.Location.get_id_from_text(from_)
-    to_id = location.Location.get_id_from_text(to_)
 
-    pipeline = get_intersac_pipeline(from_id, from_, to_id, to_, date_time)
+    pipeline = get_intersac_pipeline(from_, to_, date_time)
     result = location.Location.objects().aggregate(pipeline).next()["list"]
     logger.debug(f"Found {len(result)} connections")
+
+    # check for cancellations 
     final_conns = [conn["_id"] for conn in result if check_bitmap_for_date(date_time, conn["calendar"])]
+    cancels = cancellation.Cancellation.objects(connection_id__in=final_conns, calendar__start_date__lte=date_time, calendar__end_date__gte=date_time)
+    cancels = [cancel.connection_id for cancel in cancels if check_bitmap_for_date(date_time, cancel.calendar)]
+    final_conns = [conn for conn in final_conns if conn not in cancels]
     conn_objects = connection.Connection.objects(_id__in=final_conns)
-    # print(final_conns)
-    # find_correct_direction(conn_objects, from_id, to_id)
-    return final_conns
+
+    print(len(final_conns))
+    return conn_objects
